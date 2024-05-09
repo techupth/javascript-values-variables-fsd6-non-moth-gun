04c29ed0a073b5c26f037b4ef052f1b51e9a924f
Moth <kampanee.moth@gmail.com>
2024-05-09 13:48:44 +0700

    test: add ex-1 ex-2 ex-3 ex-4 .js

--- ex-4.js
+++ ex-4.js
@@ -2,12 +2,12 @@
 50 + 12; // Expression
 
 //Exercise #4: What is Expression Start coding here
-130 + 300;
+130 + 300;                  // Expresstion
 let zero = 1000 - 1000;
-20 * 120;
-1000 / 2;
+20 * 120;                   // Expresstion
+1000 / 2;                   // Expresstion
 typeof true;
 let name = "John";
-3 > 5;
-10 == 100;
-200;
+3 > 5;                      // Expresstion
+10 == 100;                  // Expresstion
+200;                        // Expresstion
